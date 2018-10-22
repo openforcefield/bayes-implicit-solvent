fdc4d15254075ed61b8dd2187f862549798d7ea1
Josh Fass <josh.fass@choderalab.org>
2018-10-22 15:24:02 -0400

    refactor FlatGBTyper to inherit from SMARTSTyper

--- bayes_implicit_solvent/typers.py
+++ bayes_implicit_solvent/typers.py
@@ -1,9 +1,56 @@
 import numpy as np
 from openeye import oechem
 from bayes_implicit_solvent.utils import smarts_to_subsearch
+from bayes_implicit_solvent.smarts import decorators as decorator_dict
 
+decorators = sorted(list(decorator_dict.keys()))
+def sample_decorator_uniformly_at_random():
+    return decorators[np.random.randint(len(decorators))]
 
-class FlatGBTyper():
+class SMARTSTyper():
+    def __init__(self, smarts_iter):
+        """Base class for typing schemes that use SMARTS.
+
+        Elaborations might structure the SMARTS collection as a list, tree, or other structure, and may
+        assign types / parameters in different ways (e.g. on the label of the last node visited in depth-first search
+        of the SMARTS tree, or in terms of contributions from all matches, or something along those lines).
+
+        Parameters
+        ----------
+        smarts_iter : iterable of SMARTS strings
+
+        Methods
+        -------
+        get_matches
+        """
+
+        self.smarts_iter = smarts_iter
+        self.smarts_list = list(self.smarts_iter) # flatten out the iterable
+        self.subsearches = list(map(smarts_to_subsearch, self.smarts_list))
+
+    def get_matches(self, oemol):
+        """Get a binary matrix of which atoms are hit by which smarts
+
+        Returns
+        -------
+        hits : binary array of shape (n_atoms, len(self.subsearches))
+        """
+        mol_ = oechem.OEMol(oemol)
+        n_atoms = len(list(mol_.GetAtoms()))
+
+        match_matrix = np.zeros((n_atoms, len(self.subsearches)), dtype=bool)
+
+        for i in range(len(self.subsearches)):
+            for match in self.subsearches[i].Match(mol_, False):
+                match_atoms = match.GetTargetAtoms()
+                for a in match_atoms:
+                    match_matrix[a.GetIdx(), i] = True
+        return match_matrix
+
+    def __repr__(self):
+        return 'SMARTSTyper with {} patterns: '.format(len(self.smarts_list)) + str(self.smarts_list)
+
+class FlatGBTyper(SMARTSTyper):
     def __init__(self, smarts_list):
         """List of SMARTS patterns, last match wins
 
@@ -11,30 +58,11 @@ class FlatGBTyper():
         ----------
         smarts_list : list of SMARTS strings
         """
-        self.smarts_list = smarts_list
-        self.subsearches = list(map(smarts_to_subsearch, self.smarts_list))
+        super().__init__(smarts_list)
         self.n_types = len(self.smarts_list)
 
-    def get_matches(self, oemol):
-        """Get a binary matrix of which atoms are hit by which smarts
-
-        Returns
-        -------
-        hits : binary array of shape (n_atoms, self.n_types)
-        """
-        mol_ = oechem.OEMol(oemol)
-        n_atoms = len(list(mol_.GetAtoms()))
-
-        match_matrix = np.zeros((n_atoms, self.n_types), dtype=bool)
-
-        for i in range(self.n_types):
-            for match in self.subsearches[i].Match(mol_, False):
-                match_atoms = match.GetTargetAtoms()
-                for a in match_atoms:
-                    match_matrix[a.GetIdx(), i] = True
-        return match_matrix
-
     def get_indices_of_last_matches(self, match_matrix):
+        """Process a binary match matrix into an integer array, assigning an integer to each atom."""
         assert (match_matrix.shape[1] == self.n_types)
         assert (np.all(match_matrix.sum(1) >= 1))  # every atom should be hit by at least one smarts pattern
 
@@ -47,11 +75,11 @@ class FlatGBTyper():
         """For each atom in oemol, get the index of the last SMARTS string in the list that matches it"""
 
         match_matrix = self.get_matches(oemol)
-
         gb_types = self.get_indices_of_last_matches(match_matrix)
-
         return gb_types
 
     def __repr__(self):
         return 'GBTyper with {} types: '.format(self.n_types) + str(self.smarts_list)
 
+if __name__ == '__main__':
+    pass
