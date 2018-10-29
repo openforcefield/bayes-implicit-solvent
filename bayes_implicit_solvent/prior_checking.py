a38a24eaf9071531725344407e0be77d362bcbd0
Josh Fass <josh.fass@choderalab.org>
2018-10-29 17:21:32 -0400

    Cache prior evaluations per discrete typing scheme
    Checking for unused types on a large set of molecules is not an instantaneous operation, and it's wasteful to do this during every single MCMC step, if the discrete typing scheme isn't changing.

--- bayes_implicit_solvent/prior_checking.py
+++ bayes_implicit_solvent/prior_checking.py
@@ -24,6 +24,23 @@ def check_no_decorators_applied_to_wildcard(typer):
 def check_no_duplicates(typer):
     pass
 
+def cache_based_on_discrete_typing_scheme(function):
+
+    cache = dict()
+    def cached(typer):
+
+        discrete_typer = tuple(typer.G.edges())
+
+        if discrete_typer not in cache:
+            print("looking at a scheme that's not in the cache! updating...")
+            cache[discrete_typer] = function(typer)
+
+        return cache[discrete_typer]
+
+    return cached
+
+
+@cache_based_on_discrete_typing_scheme
 def check_no_empty_types(typer):
     """Apply the typer to every molecule in all_oe_mols, and return -np.inf if the typer contains any
     non-wildcard types that aren't used"""
