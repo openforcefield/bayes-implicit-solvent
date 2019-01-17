12396568d10ef456b5ceb8ac618f9114d5be66b1
Josh Fass <josh.fass@choderalab.org>
2019-01-17 06:32:57 -0500

    refactor duplicated test setup in test_molecule to use pytest fixtures

--- bayes_implicit_solvent/tests/test_molecule.py
+++ bayes_implicit_solvent/tests/test_molecule.py
@@ -1,26 +1,30 @@
 import numpy as np
+import pytest
 
 from bayes_implicit_solvent.molecule import Molecule
 from bayes_implicit_solvent.solvation_free_energy import smiles_list, db
 
-# TODO: Refactor duplicated test setup to use pytest fixtures
-def test_molecule_gets_right_smiles():
+
+@pytest.fixture
+def initialize_molecule_list():
+    mol_smiles_pairs = []
     for _ in range(100):
         smiles = smiles_list[np.random.randint(len(smiles_list))]
         mol = Molecule(smiles, vacuum_samples=[])
+        mol_smiles_pairs.append((mol, smiles))
+    return mol_smiles_pairs
+
+
+def test_molecule_gets_right_smiles(initialize_molecule_list):
+    for (mol, smiles) in initialize_molecule_list:
         assert (smiles == mol.smiles)
 
 
-def test_molecule_consistent_with_smiles_list():
-    for _ in range(100):
-        smiles = smiles_list[np.random.randint(len(smiles_list))]
-        mol = Molecule(smiles, vacuum_samples=[])
-
+def test_molecule_consistent_with_smiles_list(initialize_molecule_list):
+    for (mol, smiles) in initialize_molecule_list:
         assert (mol.smiles == smiles_list[mol.mol_index_in_smiles_list])
 
 
-def test_molecule_consistent_with_db():
-    for _ in range(100):
-        smiles = smiles_list[np.random.randint(len(smiles_list))]
-        mol = Molecule(smiles, vacuum_samples=[])
+def test_molecule_consistent_with_db(initialize_molecule_list):
+    for (mol, smiles) in initialize_molecule_list:
         assert (mol.smiles == db[mol.mol_index_in_freesolv][1])
