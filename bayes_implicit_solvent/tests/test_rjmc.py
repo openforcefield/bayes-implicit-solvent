469a84bca5848bb3853d75fd2a8d20fd96676dfa
Josh Fass <josh.fass@choderalab.org>
2019-01-17 09:19:20 -0500

    further simplify RJMC tests

--- bayes_implicit_solvent/tests/test_rjmc.py
+++ bayes_implicit_solvent/tests/test_rjmc.py
@@ -10,9 +10,10 @@ from bayes_implicit_solvent.samplers import tree_rjmc
 from bayes_implicit_solvent.typers import GBTypingTree, AtomSpecificationProposal
 
 
-def check_self_consistency(initial_tree, max_tries=100):
+def assert_self_consistency(initial_tree, max_tries=100):
     """Up to max_tries times, sample the creation proposal, then sample the deletion proposal, and
     if you get back initial tree, confirm that log_prob_forward_over_reverse is consistent in the two directions"""
+
     for _ in range(max_tries):
         elaborated_proposal = initial_tree.sample_creation_proposal()
         elaborate_tree = elaborated_proposal['proposal']
@@ -51,7 +52,7 @@ def test_proposal_self_consistency_on_random_walk(walk_length=100):
     for _ in range(walk_length):
         traj.append(traj[-1].sample_creation_proposal()['proposal'])
     for tree in traj:
-        check_self_consistency(tree)
+        assert_self_consistency(tree)
 
 
 def test_atom_specification_proposal(n_tests=50):
@@ -60,28 +61,37 @@ def test_atom_specification_proposal(n_tests=50):
     initial_tree = construct_initial_tree()
     # adding and removing a single specifier
     for _ in range(n_tests):
-        check_self_consistency(initial_tree)
+        assert_self_consistency(initial_tree)
 
     print('depth-1 trees okay')
     # adding and removing more than one specifier
     for _ in range(n_tests):
         elaborated_proposal = initial_tree.sample_creation_proposal()['proposal']
-        check_self_consistency(elaborated_proposal)
+        assert_self_consistency(elaborated_proposal)
     print('depth-2 trees okay')
 
 
-def test_uniform_sampling(depth_cutoff=2, n_iterations=10000):
+from simtk import unit
+
+
+def test_uniform_sampling(depth_cutoff=2, n_iterations=50000):
     """Test that we get a uniform distribution over bounded-depth trees"""
 
     np.random.seed(0)
 
-    specifiers = ['X1', 'X2', 'X3']
+    # specifiers = ['X1', 'X2', 'X3']
+    specifiers = ['X1']
+
+    # TODO: Set up testing fixtures with different numbers of specifiers, depth_cutoffs, etc.
 
     atom_specification_proposal = AtomSpecificationProposal(atomic_specifiers=specifiers)
     N = len(atom_specification_proposal.atomic_specifiers)
     un_delete_able_types = ['*', '[#1]']
     initial_tree = GBTypingTree(smirks_elaboration_proposal=atom_specification_proposal,
-                                un_delete_able_types=un_delete_able_types)
+                                un_delete_able_types=un_delete_able_types,
+                                proposal_sigma=1.0 * unit.nanometer,
+                                )
+
     for base_type in un_delete_able_types[1:]:
         initial_tree.add_child(child_smirks=base_type, parent_smirks='*')
 
@@ -95,13 +105,17 @@ def test_uniform_sampling(depth_cutoff=2, n_iterations=10000):
 
         if (len(set(tree.nodes)) == tree.number_of_nodes) and \
                 (max(nx.shortest_path_length(tree.G, source='*').values()) <= depth_cutoff):
-            return 0
+            # return 0
+            return np.sum(
+                [((initial_tree.get_radius(smirks) - (0.1 * unit.nanometer)) / unit.nanometer) ** 2 for smirks in
+                 initial_tree.nodes])
         else:
             return - np.inf
 
     result = tree_rjmc(initial_tree, log_prob,
                        n_iterations=n_iterations,
-                       fraction_cross_model_proposals=0.99)
+                       fraction_cross_model_proposals=0.5,
+                       )
 
     print('number of possible distinct discrete trees at each length',
           list(zip(range(len(number_of_trees_at_each_length)), number_of_trees_at_each_length)))
@@ -113,11 +127,16 @@ def test_uniform_sampling(depth_cutoff=2, n_iterations=10000):
     print(initial_tree)
 
     traj = result['traj']
+    string_representations = list(map(str, traj))
+    print('number of distinct sampled models (as reflected in string representation)', len(set(string_representations)))
+    for s in sorted(list(set(string_representations))):
+        print(s)
+
     discrete_models = [tuple(t.ordered_nodes[2:]) for t in traj]
     distinct_discrete_models = sorted(list(set(discrete_models)))
     for d in distinct_discrete_models:
         print(d)
-    print("Number of distinct sampled models:", len(distinct_discrete_models))
+    print("number of distinct sampled models (as reflected in choice of smirks)", len(distinct_discrete_models))
 
     lengths = np.array([len(d) for d in discrete_models])
 
