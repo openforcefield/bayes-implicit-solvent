60b63d1f0c3d8c4eeffa53c1f60e1eb0b2c4df09
Josh Fass <josh.fass@choderalab.org>
2019-01-17 09:39:56 -0500

    move incomplete test to appropriate file

--- bayes_implicit_solvent/tests/test_solvation_free_energy.py
+++ bayes_implicit_solvent/tests/test_solvation_free_energy.py
@@ -46,3 +46,27 @@ def test_create_implicit_sim(alaninedipeptide_testsystem):
         if 'GBSA' not in force.__class__.__name__:
             assert ( force.getForceGroup() == 1)
 
+
+def test_get_implicit_u_diffs():
+    """check that the function based on force groups computes the same reduced potential energy differences
+    as directly computing beta * (U_implicit - U_vacuum)"""
+
+    # TODO: Finish writing this test
+    raise (NotImplementedError('must initialize vacuum_sim, implicit_sim, x'))
+    vacuum_sim, implicit_sim, x = None, None, None
+
+    u_diff = get_implicit_u_diffs(implicit_sim, x)
+
+    u_diff_ref = np.zeros(len(x))
+
+    for i in range(len(x)):
+        vacuum_sim.context.setPositions(x[i])
+        implicit_sim.context.setPositions(x[i])
+
+        U_implicit = implicit_sim.context.getState(getEnergy=True).getPotentialEnergy()
+        U_vacuum = vacuum_sim.context.getState(getEnergy=True).getPotentialEnergy()
+        u_diff_ref[i] = beta * (U_implicit - U_vacuum)
+
+        u_diff_ref[i] = beta * implicit_sim.context.getState(getEnergy=True, groups={0}).getPotentialEnergy()
+
+    np.testing.assert_allclose(u_diff, u_diff_ref)
