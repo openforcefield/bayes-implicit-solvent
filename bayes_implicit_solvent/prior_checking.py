9e15c0cf4c13c8229b946780e7a21a8735119ee8
Josh Fass <josh.fass@choderalab.org>
2019-02-20 13:01:01 -0500

    reformat prior_checking.py

--- bayes_implicit_solvent/prior_checking.py
+++ bayes_implicit_solvent/prior_checking.py
@@ -7,25 +7,38 @@ We will also enforce that every SMIRKS pattern is valid.
 Some checks will make reference to a fixed library of compounds,
 for example, checking that a typing scheme doesn't have any unused types or redundant types
 """
+from functools import lru_cache
+
 import numpy as np
+
 from bayes_implicit_solvent.solvation_free_energy import mol_top_sys_pos_list
+
 all_oe_mols = [entry[0] for entry in mol_top_sys_pos_list]
+
+
 # TODO: Replace with minidrugbank or something
 
+# TODO: Implement check_valid_smirks
 def check_valid_smirks(smirks_string):
     pass
 
+
+# TODO: Implement check_all_valid_smirks
 def check_all_valid_smirks(typer):
     pass
 
+
+# TODO: Implement check no decorators applied to wildcrd
 def check_no_decorators_applied_to_wildcard(typer):
     pass
 
+
+# TODO: Implement check no duplicates
 def check_no_duplicates(typer):
     pass
 
-from functools import lru_cache
-@lru_cache(maxsize=2**12)
+
+@lru_cache(maxsize=2 ** 12)
 def check_no_empty_types(typer):
     """Apply the typer to every molecule in all_oe_mols, and return -np.inf if the typer contains any
     non-wildcard types that aren't used"""
@@ -36,9 +49,8 @@ def check_no_empty_types(typer):
     # check that no type is unused
     counts = np.bincount(flat, minlength=N)
     if np.min(counts[1:]) == 0:  # TODO: revisit [1:] slice if we change how wildcard is handled
-        #print('empty types found!')
-        #print([typer.ordered_nodes[i] for i in range(len(counts)) if counts[i] == 0])
+        # print('empty types found!')
+        # print([typer.ordered_nodes[i] for i in range(len(counts)) if counts[i] == 0])
         return -np.inf
     else:
         return 0
-
