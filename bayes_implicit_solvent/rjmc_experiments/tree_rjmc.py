2f52a6e6a14c5dcfeaa614caff870c7e05ab55d6
Josh Fass <josh.fass@choderalab.org>
2019-02-27 13:30:22 -0500

    update tree_rjmc script to initialize from obc2_model

--- bayes_implicit_solvent/rjmc_experiments/tree_rjmc.py
+++ bayes_implicit_solvent/rjmc_experiments/tree_rjmc.py
@@ -1,46 +1,40 @@
-import mdtraj as md
 import numpy as np
-from pkg_resources import resource_filename
 
-from bayes_implicit_solvent.constants import RADIUS_UNIT
-from bayes_implicit_solvent.molecule import Molecule
-from bayes_implicit_solvent.smarts import atomic_number_dict
-from bayes_implicit_solvent.solvation_free_energy import smiles_list
-from bayes_implicit_solvent.typers import GBTypingTree
-from bayes_implicit_solvent.utils import mdtraj_to_list_of_unitted_snapshots
+from bayes_implicit_solvent.continuous_parameter_experiments.elemental_types_mh import log_prior, mols, ll, data_path, \
+    smiles
 
-data_path = '../data/'
+smiles_list = smiles
+from bayes_implicit_solvent.typers import RADIUS_UNIT
+
+from bayes_implicit_solvent.freesolv import smiles_list
+from bayes_implicit_solvent.typers import AtomSpecificationProposal
 
 np.random.seed(0)
 
-un_delete_able_types = ['*']
-for base_type in atomic_number_dict.keys():
-    un_delete_able_types.append(base_type)
-initial_tree = GBTypingTree(un_delete_able_types=un_delete_able_types)
-for base_type in un_delete_able_types[1:]:
-    initial_tree.add_child(child_smirks=base_type, parent_smirks='*')
+from bayes_implicit_solvent.gb_models.obc2_parameters import obc2_model
+
+initial_tree = obc2_model
+
+# # add one more parameter per element appearing in FreeSolv but not specified in obc2 parameter set to initial tree
+# for i in [17, 35, 53]:
+#    initial_tree.add_child('[#{}]'.format(i), '*')
+
+specifiers = ['X1', 'X2', 'X3', 'X4', 'a', 'A']
+atom_specification_proposal = AtomSpecificationProposal(atomic_specifiers=specifiers)
+smirks_elaboration_proposal = atom_specification_proposal
 
 print('initial tree:')
 print(initial_tree)
 
-mols = []
+n_configuration_samples = 25
 
-smiles_subset = list(smiles_list)
-np.random.shuffle(smiles_subset)
-smiles_subset = smiles_subset[:int(len(smiles_list) * 0.75)]
-print('looking at {} entries from FreeSolv'.format(len(smiles_subset)))
-n_configuration_samples = 10
+import os
 
-for smiles in smiles_subset:
-    mol = Molecule(smiles, vacuum_samples=[])
-    path_to_vacuum_samples = resource_filename('bayes_implicit_solvent',
-                                               'vacuum_samples/vacuum_samples_{}.h5'.format(
-                                                   mol.mol_index_in_smiles_list))
-    vacuum_traj = md.load(path_to_vacuum_samples)
-    thinning = int(len(vacuum_traj) / n_configuration_samples)
-    mol.vacuum_traj = mdtraj_to_list_of_unitted_snapshots(vacuum_traj[::thinning])
-    print('thinned vacuum_traj from {} to {}'.format(len(vacuum_traj), len(mol.vacuum_traj)))
-    mols.append(mol)
+name = 'tree_rjmc_n_config={}_{}_ll'.format(n_configuration_samples, ll)
+smiles_subset_fname = os.path.join(data_path,
+                                   'smiles_subset_{}.txt'.format(name))
+with open(smiles_subset_fname, 'w') as f:
+    f.writelines(['{}\n'.format(s) for s in smiles_list])
 
 from bayes_implicit_solvent.prior_checking import check_no_empty_types
 
@@ -48,32 +42,40 @@ error_y_trees = []
 
 
 def log_prob(tree):
-    log_prior = check_no_empty_types(tree)
+    log_prior_value = check_no_empty_types(tree)
 
-    if log_prior > -np.inf:
+    theta = np.hstack([tree.get_radii(), tree.get_scale_factors()])
+
+    log_prior_value = log_prior(theta)
+
+    if log_prior_value > -np.inf:
         try:
             # TODO: Parallelize. Note that multiprocessing.Pool won't work here because it doesn't play nice with SwigPy objects
             # TODO: update to allow scale factors to be variable also
-            log_prob_components = [mol.log_prob(tree.assign_radii(mol.mol) / RADIUS_UNIT, np.ones(mol.n_atoms)) for mol in mols]
-            log_posterior = sum(log_prob_components)
+            log_likelihood_value = 0
+            for mol in mols:
+                radii = tree.assign_radii(mol.mol) / RADIUS_UNIT
+                scale_factors = tree.assign_scale_factors(mol.mol)
+
+                log_likelihood_value += mol.log_prob(radii, scale_factors)
         except:
             global error_y_trees
             error_y_trees.append(tree)
             print('Warning! Encountered un-anticipated exception!')
             return - np.inf
-        # return sum([mol.log_prob(tree.assign_radii(mol.mol)) for mol in mols])
-        return log_prior + log_posterior
+        return log_prior_value + log_likelihood_value
     else:
-        return log_prior
+        return log_prior_value
 
 
 from bayes_implicit_solvent.samplers import tree_rjmc
 from pickle import dump
 
-n_iterations = 200000
+n_iterations = 1000
 
-result = tree_rjmc(initial_tree, log_prob, n_iterations=n_iterations)
-with open('elaborate_tree_rjmc_run_n_compounds={}_n_iter={}_gaussian_ll.pkl'.format(len(smiles_subset), n_iterations),
+result = tree_rjmc(initial_tree, log_prob, smirks_elaboration_proposal, n_iterations=n_iterations,
+                   fraction_cross_model_proposals=0.15)
+with open('elaborate_tree_rjmc_run_n_compounds={}_n_iter={}_gaussian_ll.pkl'.format(len(smiles_list), n_iterations),
           'wb') as f:
     dump(result, f)
 
