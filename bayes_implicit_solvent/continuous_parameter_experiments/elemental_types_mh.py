03f217d8adec8d53d6b4f095bd16274b17809ada
Josh Fass <josh.fass@choderalab.org>
2019-02-27 08:16:59 -0500

    update elelental_types_mh example

--- bayes_implicit_solvent/continuous_parameter_experiments/elemental_types_mh.py
+++ bayes_implicit_solvent/continuous_parameter_experiments/elemental_types_mh.py
@@ -1,232 +1,127 @@
-# use metropolis-adjusted langevin to sample the radii and scales in
-# the elemental-types-only model
-
-
 import os.path
 
-import mdtraj as md
-from autograd import numpy as np
-from pkg_resources import resource_filename
+import numpy as np
+from bayes_implicit_solvent.molecule import Molecule
 from simtk import unit
 
-from bayes_implicit_solvent.molecule import Molecule
 from bayes_implicit_solvent.samplers import sparse_mh
-from bayes_implicit_solvent.solvation_free_energy import smiles_list
-from bayes_implicit_solvent.utils import mdtraj_to_list_of_unitted_snapshots
 
-data_path = '../data/'
-gaussian_ll = True
 
-import sys
+def sample_path_to_unitted_snapshots(path_to_npy_samples):
+    xyz = np.load(path_to_npy_samples)
+    traj = [snapshot * unit.nanometer for snapshot in xyz]
+    return traj
 
-try:
-    job_id = int(sys.argv[1])
-    print("Using supplied job_id: ", job_id)
-except:
-    job_id = np.random.randint(1000)
-    print("No valid job_id supplied! Selecting one at random: ", job_id)
 
-# 1. load the dataset, precompute distance matrices
-np.random.seed(job_id)
+from glob import glob
+from pkg_resources import resource_filename
 
-inds = np.arange(len(smiles_list))
-np.random.shuffle(inds)
-inds = inds[:int(len(smiles_list) * 0.5)]
-smiles_subset = [smiles_list[i] for i in inds]
+data_path = resource_filename('bayes_implicit_solvent',
+                              'data')
+ll = 'gaussian'  # or 'student-t'
+randomize_theta0 = False
+n_conf = 25
+
+path_to_vacuum_samples = resource_filename('bayes_implicit_solvent',
+                                           'vacuum_samples/short_run/vacuum_samples_*.npy')
+paths_to_samples = glob(path_to_vacuum_samples)
+np.random.seed(0)
+np.random.shuffle(paths_to_samples)
+paths_to_samples = paths_to_samples[::2]
+
+print('number of molecules being considered: {}'.format(len(paths_to_samples)))
+
+
+def extract_cid_key(path):
+    i = path.find('mobley_')
+    j = path.find('.npy')
+    return path[i:j]
+
+
+cids = list(map(extract_cid_key, paths_to_samples))
+print('first few CIDs', cids[:5])
 
 mols = []
-elements = []
-charges = []
-distance_matrices = []
-expt_means = []
-expt_uncs = []
-vacuum_trajs = []
 
-n_configuration_samples = 5  # TODO: Since this is cheaper, can probably modify this a bit...
+n_configuration_samples = n_conf  # TODO: Since this is cheaper, can probably modify this a bit...
 
-name = 'n_config={}_job_id={}'.format(n_configuration_samples, job_id)
-if gaussian_ll:
-    name = name + '_gaussian_ll'
-smiles_subset_fname = os.path.join(data_path,
-                                   'smiles_subset_{}.txt'.format(name))
-with open(smiles_subset_fname, 'w') as f:
-    f.writelines(['{}\n'.format(s) for s in smiles_subset])
+name = 'n_config={}_{}_ll'.format(n_configuration_samples, ll)
 
-from bayes_implicit_solvent.utils import get_charges
-from scipy.spatial.distance import pdist, squareform
+from bayes_implicit_solvent.freesolv import cid_to_smiles
 
-for smiles in smiles_subset:
-    mol = Molecule(smiles, vacuum_samples=[])
-    path_to_vacuum_samples = resource_filename('bayes_implicit_solvent',
-                                               'vacuum_samples/vacuum_samples_{}.h5'.format(
-                                                   mol.mol_index_in_smiles_list))
-    vacuum_traj = md.load(path_to_vacuum_samples)
-    thinning = int(len(vacuum_traj) / n_configuration_samples)
-    mol.vacuum_traj = mdtraj_to_list_of_unitted_snapshots(vacuum_traj[::thinning])
-    vacuum_trajs.append(mol.vacuum_traj)
-    print('thinned vacuum_traj from {} to {}'.format(len(vacuum_traj), len(mol.vacuum_traj)))
-
-    expt_means.append(mol.experimental_value)
-    expt_uncs.append(mol.experimental_uncertainty)
-
-    elements.append(np.array([a.element.atomic_number for a in mol.top.atoms()]))
-    charges.append(get_charges(mol.sys))
-    distance_matrices.append([squareform(pdist(snapshot / unit.nanometer)) for snapshot in mol.vacuum_traj])
+for path in paths_to_samples:
+    cid = extract_cid_key(path)
+    smiles = cid_to_smiles[cid]
+    vacuum_samples = sample_path_to_unitted_snapshots(path)
+    thinning = int(len(vacuum_samples) / n_configuration_samples)
+    mol = Molecule(smiles, vacuum_samples=vacuum_samples[::thinning], ll=ll)
     mols.append(mol)
+
 # 2. Define a likelihood function, including "type-assignment"
-from autograd import numpy as np
-from autograd.scipy.stats import norm
-from autograd.scipy.stats import t as student_t
-from autograd.scipy.misc import logsumexp
-from simtk import unit
-from bayes_implicit_solvent.constants import kB, temperature
 
-kj_mol_to_kT = 1.0 * unit.kilojoule_per_mole / (kB * temperature)
+from bayes_implicit_solvent.gb_models.obc2_parameters import obc2_model
 
-
-def one_sided_exp(w_F):
-    DeltaF = - (logsumexp(- w_F) - np.log(len(w_F)))
-    return DeltaF
-
-
-# TODO: parallelize using multiprocessing
-
-all_elements = [1, 6, 7, 8, 9, 15, 16, 17, 35, 53]
-element_dict = dict(zip(all_elements, list(range(len(all_elements)))))
-
-# default radii are 0.15
-initial_radius_dict = {}
-for atomic_number in all_elements:
-    initial_radius_dict[atomic_number] = 0.15
-
-# a few elements have different radii in the default model
-# https://github.com/pandegroup/openmm/blob/master/wrappers/python/simtk/openmm/app/internal/customgbforces.py#L233-L257
-initial_radius_dict[1] = 0.12
-initial_radius_dict[6] = 0.17
-initial_radius_dict[7] = 0.155
-initial_radius_dict[14] = 0.21
-initial_radius_dict[15] = 0.185
-initial_radius_dict[16] = 0.18
-
-initial_radii = np.array([initial_radius_dict[a] for a in all_elements])
-
-# scales
-initial_scale_dict = {}
-for atomic_number in all_elements:
-    initial_scale_dict[atomic_number] = 0.8
-
-# a few elements have different radii in the default model
-# https://github.com/pandegroup/openmm/blob/master/wrappers/python/simtk/openmm/app/internal/customgbforces.py#L233-L257
-initial_scale_dict[1] = 0.85
-initial_scale_dict[6] = 0.72
-initial_scale_dict[7] = 0.79
-initial_scale_dict[8] = 0.85
-initial_scale_dict[9] = 0.88
-initial_scale_dict[15] = 0.86
-initial_scale_dict[16] = 0.96
-
-initial_scales = np.array([initial_scale_dict[a] for a in all_elements])
-
-
-def pack(radii, scales):
-    n = len(radii)
-    theta = np.zeros(2 * n)
-    theta[:n] = radii
-    theta[n:2 * n] = scales
-    return theta
-
-
-def unpack(theta):
-    n = int((len(theta)) / 2)
-    radii, scales = theta[:n], theta[n:2 * n]
-    return radii, scales
-
-
-def construct_array(i, theta):
-    radii, scales = unpack(theta)
-
-    mol_radii = np.array([radii[element_dict[element]] for element in elements[i]])
-    mol_scales = np.array([scales[element_dict[element]] for element in elements[i]])
-    return mol_radii, mol_scales
+type_slices = [obc2_model.apply_to_molecule(mol.mol) for mol in mols]
 
 
 def construct_arrays(theta):
-    arrays = [construct_array(i, theta) for i in range(len(mols))]
-    mol_radii = [a[0] for a in arrays]
-    mol_scales = [a[1] for a in arrays]
-    return mol_radii, mol_scales
+    n = int(len(theta) / 2)
+    radii, scales = theta[:n], theta[n:]
+
+    parameterized_list = []
+    for i in range(len(mols)):
+        parameterized_list.append((radii[type_slices[i]], scales[type_slices[i]]))
+    return parameterized_list
 
 
-from bayes_implicit_solvent.gb_models.numpy_gb_models import compute_OBC_energy_vectorized
+from bayes_implicit_solvent.constants import min_r, max_r, min_scale, max_scale
 
 
-def log_prob_component(i, theta):
-    radii, scales = construct_array(i, theta)
-    if min(np.min(radii), np.min(scales)) < 0.001:
-        #print('out of bounds!')
-        return -np.inf
-    W_F = np.array([compute_OBC_energy_vectorized(distance_matrix, radii, scales, charges[i]) for distance_matrix in
-                    distance_matrices[i]])
-    w_F = W_F * kj_mol_to_kT
-    pred_free_energy = one_sided_exp(w_F)
-    if gaussian_ll:
-        return norm.logpdf(pred_free_energy, loc=expt_means[i], scale=expt_uncs[i] ** 2)
+def log_prior(theta):
+    n = int(len(theta) / 2)
+    radii, scales = theta[:n], theta[n:]
+
+    if (min(radii) < min_r) or (max(radii) > max_r) or (min(scales) < min_scale) or (max(scales) > max_scale):
+        return - np.inf
     else:
-        return student_t.logpdf(pred_free_energy, loc=expt_means[i],
-                                scale=expt_uncs[i] ** 2,
-                                df=7)
+        return 0
 
 
 def log_prob(theta):
-    mol_radii, mol_scales = construct_arrays(theta)
-
-    if np.min(theta) < 0.001 or np.max(theta) > 2:
-        #print('out of bounds!')
-        return -np.inf
-    logp = 0
-    for i in range(len(mols)):
-        radii = mol_radii[i]
-        scales = mol_scales[i]
-        W_F = np.array([compute_OBC_energy_vectorized(distance_matrix, radii, scales, charges[i]) for distance_matrix in
-                        distance_matrices[i]])
-        w_F = W_F * kj_mol_to_kT
-        pred_free_energy = one_sided_exp(w_F)
-
-        if gaussian_ll:
-            logp += norm.logpdf(pred_free_energy, loc=expt_means[i], scale=expt_uncs[i] ** 2)
-        else:
-            logp += student_t.logpdf(pred_free_energy, loc=expt_means[i],
-                                     scale=expt_uncs[i] ** 2,
-                                     df=7)
-    return logp
+    L = log_prior(theta)
+    if not (L > -np.inf):
+        return L
+    else:
+        parameterized_list = construct_arrays(theta)
+        for i, mol in enumerate(mols):
+            radii, scale_factors = parameterized_list[i]
+            L += mol.log_prob(radii, scale_factors)
+    return L
 
 
-from functools import partial
 if __name__ == '__main__':
-    from multiprocessing import Pool
+    n_types = obc2_model.number_of_nodes
+    print('n_types: {}'.format(n_types))
 
-    n_processes = 4
-    pool = Pool(n_processes)
+    initial_radii = np.array(obc2_model.get_radii())
+    initial_scales = np.array(obc2_model.get_scale_factors())
+    theta0 = np.hstack((initial_radii, initial_scales))
 
-
-    def parallel_log_prob(theta):
-        return sum(pool.map(partial(log_prob_component, theta=theta), range(len(vacuum_trajs))))
-
-
-    theta0 = pack(initial_radii, initial_scales)
     print('initial theta', theta0)
-    initial_log_prob = parallel_log_prob(theta0)
-    print('initial log prob', log_prob(theta0))
+    initial_log_prob = log_prob(theta0)
 
+    minimized_theta_fname = os.path.join(data_path,
+                                         'elemental_types_l-bfgs-finite-difference_{}.npy'.format(
+                                             name))
     stepsize = 0.5 * 1e-1
     n_steps = 10000
     dim_to_perturb = 5
 
-    traj, log_probs, acceptance_fraction = sparse_mh(theta0, parallel_log_prob, n_steps=n_steps, stepsize=stepsize,
+    traj, log_probs, acceptance_fraction = sparse_mh(theta0, log_prob, n_steps=n_steps, stepsize=stepsize,
                                                      dim_to_perturb=dim_to_perturb)
 
     np.savez(os.path.join(data_path,
                           'elemental_types_mh_freesolv_{}.npz'.format(
                               name)),
              traj=traj, log_probs=log_probs, acceptance_fraction=acceptance_fraction, stepsize=stepsize,
-             n_steps=n_steps, dim_to_perturb=dim_to_perturb)
+             n_steps=n_steps, dim_to_perturb=dim_to_perturb, cids=cids, n_configuration_samples=n_conf)
