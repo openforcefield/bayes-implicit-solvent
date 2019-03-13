3e937ece824843a9743a8c8f531cd1c96237ec16
Josh Fass <josh.fass@choderalab.org>
2019-03-13 17:37:22 -0400

    update two-model RJMC test, sweep over proposal sigmas also

--- bayes_implicit_solvent/marginal_likelihood/rjmc_test.py
+++ bayes_implicit_solvent/marginal_likelihood/rjmc_test.py
@@ -5,20 +5,25 @@ import numpy as np
 from bayes_implicit_solvent.typers import GBTypingTree
 from bayes_implicit_solvent.typers import RADIUS_UNIT
 
-initial_tree = GBTypingTree()
-initial_tree.proposal_sigmas['radius'] = 1e-2 * RADIUS_UNIT
-initial_tree.proposal_sigmas['scale_factor'] = 1e-2
+from bayes_implicit_solvent.typers import DiscreteProposal
+class RootSpecificationProposal(DiscreteProposal):
+    def __init__(self, elaborations):
+        self.elaborations = elaborations
 
-from bayes_implicit_solvent.typers import AtomSpecificationProposal, SMIRKSElaborationProposal
+    def log_prob_forward(self, initial_smirks, proposed_smirks=None):
+        return - np.log(len(self.elaborations)) # since I can only elaborate on the root
 
-atom_specification_proposal = AtomSpecificationProposal(atomic_specifiers=['[#6]'])
-smirks_elaborators = [
-    atom_specification_proposal,
-]
-smirks_elaboration_proposal = SMIRKSElaborationProposal(smirks_elaborators=smirks_elaborators)
+    def sample(self, initial_smirks):
+        assert(initial_smirks == '*')
+        proposal_smirks = self.elaborations[np.random.randint(len(self.elaborations))]
 
-print('initial tree:')
-print(initial_tree)
+        return {
+            'proposal': proposal_smirks,
+            'log_prob_forward_over_reverse': self.log_prob_forward_over_reverse(initial_smirks, proposal_smirks),
+        }
+
+smirks_elaboration_proposal = RootSpecificationProposal(['[#6]'])
+
 
 from bayes_implicit_solvent.marginal_likelihood.single_type_forward_ais import log_posterior as log_posterior_one_type
 from bayes_implicit_solvent.marginal_likelihood.two_types_radius_and_scale_forward_ais import \
@@ -41,12 +46,46 @@ def log_prob(tree):
 
 
 from bayes_implicit_solvent.samplers import tree_rjmc
-from pickle import dump
 
 n_iterations = 10000
+proposal_sigmas = [1e-3, 5e-3, 1e-2, 5e-2, 1e-1]
+n_types_trajs = []
+tree_trajs = []
+log_prob_trajs = []
+log_accept_prob_trajs = []
+for proposal_sigma in proposal_sigmas:
+    initial_tree = GBTypingTree(max_nodes=2)
+    initial_tree.proposal_sigmas['radius'] = proposal_sigma * RADIUS_UNIT
+    initial_tree.proposal_sigmas['scale_factor'] = proposal_sigma
+    initial_tree.is_decorate_able = lambda \
+        node: node == '*'  # overwrite method that currently returns False if the node is the root node...
 
-result = tree_rjmc(initial_tree, log_prob, smirks_elaboration_proposal, n_iterations=n_iterations,
-                   fraction_cross_model_proposals=0.1)
-with open('simple_tree_rjmc_run_n_compounds={}_n_iter={}_gaussian_ll.pkl'.format(len(mols), n_iterations),
-          'wb') as f:
-    dump(result, f)
+    # TODO: clue: the amount of time it spends in each model may be related to proposal sigmas
+    # TODO: clue: I see the progress bar is saying `avg. accept. prob.=nan` but it hasn't resulted in an error!
+    # initial_tree.add_child('[#6]', '*')
+    print('initial tree:')
+    print(initial_tree)
+
+    result = tree_rjmc(initial_tree, log_prob, smirks_elaboration_proposal, n_iterations=n_iterations,
+                       fraction_cross_model_proposals=0.25)
+    traj = result["traj"]
+    log_probs = result["log_probs"]
+    log_acceptance_probabilities = result["log_acceptance_probabilities"]
+
+    n_types_trajs.append(np.array([tree.number_of_nodes for tree in result['traj']]))
+    tree_trajs.append(traj)
+    log_prob_trajs.append(log_probs)
+    log_accept_prob_trajs.append(log_acceptance_probabilities)
+
+    print('proposal_sigma={}'.format(proposal_sigma))
+    print('time spent in each model: n=1: {}, n=2: {}'.format(sum(n_types_trajs[-1] == 1), sum(n_types_trajs[-1] == 2)))
+
+np.savez('rjmc_test.npz',
+         proposal_sigmas=proposal_sigmas,
+         n_types_trajs=n_types_trajs,
+         log_prob_trajs=log_prob_trajs,
+         log_accept_prob_trajs=log_accept_prob_trajs,
+         tree_strings=[[str(tree) for tree in traj] for traj in tree_trajs],
+         radii=[[tree.get_radii() for tree in traj] for traj in tree_trajs],
+         scales=[[tree.get_scale_factors() for tree in traj] for traj in tree_trajs],
+         )
