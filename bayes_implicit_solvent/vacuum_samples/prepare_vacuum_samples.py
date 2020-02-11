4b2b17edfb8bd7f95f40a3bc4f6157f9e77902ca
Josh Fass <josh.fass@choderalab.org>
2020-02-11 12:23:35 -0500

    Update prepare_vacuum_samples.py

--- bayes_implicit_solvent/vacuum_samples/prepare_vacuum_samples.py
+++ bayes_implicit_solvent/vacuum_samples/prepare_vacuum_samples.py
@@ -1,27 +1,27 @@
-from bayes_implicit_solvent.solvation_free_energy import get_vacuum_samples, mol_top_sys_pos_list
+from bayes_implicit_solvent.solvation_free_energy import get_vacuum_samples
+from bayes_implicit_solvent.freesolv import mol_top_sys_pos_list
 import numpy as np
 import mdtraj as md
 from simtk import unit
 
-n_samples = 10000
+n_samples = 100
 thinning = 1000
 
 
 if __name__ == '__main__':
-    import sys
 
-    try:
-        job_id = int(sys.argv[1])
-    except:
-        print("No valid job_id supplied! Selecting one at random")
-        job_id = np.random.randint(len(mol_top_sys_pos_list)) + 1
+    from time import time
+    t0 = time()
+    for i in range(len(mol_top_sys_pos_list)):
 
-    print(job_id)
-    mol, top, sys, pos = mol_top_sys_pos_list[job_id - 1]
+        print('molecule {} of {}'.format(i, len(mol_top_sys_pos_list)))
+        print('total elapsed time: {:.4f}s'.format(time() - t0))
 
-    vacuum_sim, vacuum_traj = get_vacuum_samples(top, sys, pos,
-                                                 n_samples=n_samples, thinning=thinning)
+        mol, top, sys, pos = mol_top_sys_pos_list[i]
 
-    xyz = np.array([snapshot / unit.nanometer for snapshot in vacuum_traj])
-    vacuum_traj = md.Trajectory(xyz, md.Topology().from_openmm(top))
-    vacuum_traj.save_hdf5('vacuum_samples_{}.h5'.format(job_id - 1))
+        vacuum_sim, vacuum_traj = get_vacuum_samples(top, sys, pos,
+                                                     n_samples=n_samples, thinning=thinning)
+
+        xyz = np.array([snapshot / unit.nanometer for snapshot in vacuum_traj])
+        vacuum_traj = md.Trajectory(xyz, md.Topology().from_openmm(top))
+        vacuum_traj.save_hdf5('vacuum_samples_{}.h5'.format(i))
